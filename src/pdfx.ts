111cbb3837bf30b5515dc3d5221667fec049e32a
Ankur Dutta <ankur.611@gmail.com>
2024-01-08 17:34:03 +0530

    PDFJs integrated

--- src/pdfx.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement, css } from "lit";
-import { customElement } from "lit/decorators.js";
-
-@customElement("pdfx-main")
-export class PdfxMain extends LitElement {
-  static styles = css``;  
-
-  
-}
