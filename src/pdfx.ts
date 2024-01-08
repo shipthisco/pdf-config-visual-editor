81b6b962337b8f55fb688e1ae8dfdefca0d2e49d
Ankur Dutta <ankur.611@gmail.com>
2024-01-08 11:13:41 +0530

    pdfx created

---
+++ src/pdfx.ts
@@ -0,0 +1,9 @@
+import { LitElement, css } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("pdfx-main")
+export class PdfxMain extends LitElement {
+  static styles = css``;  
+
+  
+}
