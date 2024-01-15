9219839953958bb29e321040e150aff8a92a7246
Ankur Dutta <ankur.611@gmail.com>
2024-01-15 12:45:06 +0530

    Result viewer working

--- src/pdf-viewer.ts
+++ src/pdf-viewer.ts
@@ -1,12 +1,10 @@
 import { LitElement, css, html } from "lit";
-import interact from 'interactjs'
 import { customElement, property, state } from "lit/decorators.js";
 import { PDFDocumentProxy, GlobalWorkerOptions, getDocument, PageViewport } from "pdfjs-dist";
-import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
 
-@customElement('pop-editor')
-class PopEditor extends LitElement {
+@customElement('pdf-viewer')
+class PdfEditor extends LitElement {
   static styles = css`
   :host {
     display: block; 
@@ -22,13 +20,14 @@ class PopEditor extends LitElement {
     color: white;
   }
 
-  #the-canvas {
+  #pdf-viewer-canvas {
     border: 1px solid black;
   }
   `
 
   @property() data: any;
   @property() url: string = '';
+
   @property({type: Number}) scale = 1;
   @property({type: Number}) rotation = 0;
   @property({type: Number}) pageNum = 1; //must be number
@@ -78,7 +77,7 @@ class PopEditor extends LitElement {
       if (!this.shadowRoot) {
         return;
       }
-      this.canvas = this.shadowRoot.getElementById('the-canvas');
+      this.canvas = this.shadowRoot.getElementById('pdf-viewer-canvas');
       const canvasContext = this.canvas.getContext("2d");
       this.canvas.height = viewport.height;
       this.canvas.width = viewport.width;
@@ -93,21 +92,6 @@ class PopEditor extends LitElement {
       // Wait for rendering to finish
       renderTask.promise.then(() => {
         console.log('Page rendered')
-
-        interact('.draggable').draggable({
-          listeners: {
-            start: (event) => {
-              console.log(event.type, event.target)
-            },
-            move: (event) => {
-              this.position.x += event.dx
-              this.position.y += event.dy
-        
-              event.target.style.transform =
-                `translate(${this.position.x}px, ${this.position.y}px)`
-            },
-          }
-        })
       });
     });
   };
@@ -121,10 +105,7 @@ class PopEditor extends LitElement {
   };
 
   initialLoad() {
-    const loadingTask = getDocument({
-      ...(this.url && { url: this.url }),
-      ...(this.data && { data: this.data }),
-    });
+    const loadingTask = getDocument({data: atob(this.data)});
 
     loadingTask.promise
       .then(async (pdfDoc_: PDFDocumentProxy) => {
@@ -135,76 +116,10 @@ class PopEditor extends LitElement {
       })
   };
 
-  getCoordinates() {
-    const pos = {
-      x: this.position.x,
-      y: this.position.y,
-    }
-    if (this.viewport?.viewBox?.length > 0) {
-      const [x, y, width, height] = this.viewport?.viewBox;
-      pos.y = height - ((pos.y * height) / this.viewport?.height);
-      pos.x = (pos.x * width) / this.viewport?.width;
-    }
-    return pos
-  }
-
-  async modifyPdf() {
-    const url = this.url;
-    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
-  
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    
-    const pos = this.getCoordinates();
-    const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const { width, height } = firstPage.getSize()
-
-    firstPage.drawText('This text was added with JavaScript!', {
-      x: pos.x,
-      y: pos.y,
-      size: 11,
-      font: helveticaFont,
-      color: rgb(0.95, 0.1, 0.1),
-      // rotate: degrees(-45),
-    })
-  
-    const pdfBytes = await pdfDoc.saveAsBase64()
-    return pdfBytes;
-  }
-
-  private async _display(e: Event) {
-    const data = await this.modifyPdf();
-    const blob = new Blob([data]);
-    const options = {
-      detail: {data, blob},
-      bubbles: true,
-      composed: true
-    };
-    this.dispatchEvent(new CustomEvent('modified', options));
-  }
-
-  private async _download(e: Event) {
-    const data = await this.modifyPdf();
-    const blob = new Blob([data]);
-
-    if (this.shadowRoot) {
-      const a = document.createElement('a');
-      this.shadowRoot.append(a);
-      a.download = 'abc.pdf';
-      a.href = URL.createObjectURL(blob);
-      a.click();
-      a.remove();
-    }
-  }
-
   // Render the UI as a function of component state
   render() {
     return html`
-      <div class="draggable"></div>
-      <canvas id="the-canvas"></canvas>
-      <button @click="${this._download}">Download</button>
-      <button @click="${this._display}">Display</button>
+      <canvas id="pdf-viewer-canvas"></canvas>
     `;
   }
 }
